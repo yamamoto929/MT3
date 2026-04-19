bea7440fd9712ddb688d985c85a57177da046ab6
Yamamoto Runa <k025g0073@g.neec.ac.jp>
2026-04-19 15:48:47 +0900

    保存

--- MT3/Functions.cpp
+++ MT3/Functions.cpp
@@ -1,17 +1,4 @@
 ﻿#include "Functions.h"
-Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
-	Matrix4x4 result{};
-	for (size_t ai = 0; ai < 4; ai++) {
-		for (size_t bi = 0; bi < 4; bi++) {
-			result.m[ai][bi] =
-				m1.m[ai][0] * m2.m[0][bi] +
-				m1.m[ai][1] * m2.m[1][bi] +
-				m1.m[ai][2] * m2.m[2][bi] +
-				m1.m[ai][3] * m2.m[3][bi];
-		}
-	}
-	return result;
-};
 
 Matrix4x4 MakeRotateXMatrix(float radian) {
 	Matrix4x4 result = { {
--- MT3/main.cpp
+++ MT3/main.cpp
@@ -1,17 +1,35 @@
 ﻿#include <Novice.h>
+#include "Functions.h"
 
 const char kWindowTitle[] = "LE2B_30_ヤマモト_ルナ_MT3_01_01";
+const int kWindowWidth = 1280;
+const int kWindowHeight = 720;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	Matrix4x4 worldMatrix = MakeAffineMatrix(...);
+	Matrix4x4 cameraMatrix = MakeAffineMatrix(...);
+	Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+	Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(...);
+	// WVPMatrixを作る
+	Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+	// ViewportMatrixを作る
+	Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, 1280.0f, 720.0f, 0.0f, 1.0f);
+	// Screen空間へと頂点を変換する
+	Vector3 screenVertices[3];
+	for (uint32_t i = 0; i < 3; ++i) {
+		// NDCまで変換。Transformを使うと同次座標系->デカルト座標系の処理が行われ、結果的にZDivideが行われることになる
+		Vector3 ndcVertex = Transform(kLocalVertices[i], worldViewProjectionMatrix);
+
+	}
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始

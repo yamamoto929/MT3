5bb79b07832a1446838b8be10fa68d14dcfc484d
Yamamoto Runa <k025g0073@g.neec.ac.jp>
2026-04-19 15:55:21 +0900

    保存

--- MT3/Functions.h
+++ MT3/Functions.h
@@ -7,10 +7,19 @@
 static const inline int kColumnWidth = 60;
 static const inline int kRowHeight = 20;
 
-Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+// 透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovy, float aspectRatio, float nearClip, float farClip);
+
+// 正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+// ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+
 Matrix4x4 MakeRotateXMatrix(float radian);
 Matrix4x4 MakeRotateYMatrix(float radian);
 Matrix4x4 MakeRotateZMatrix(float radian);
 Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
+
+float Cot(float a);

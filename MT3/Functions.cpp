f447bd0d924008d7712f83e3562eff9935bed0eb
Yamamoto Runa <k025g0073@g.neec.ac.jp>
2026-04-24 10:52:37 +0900

    MT3_01_01_3

--- MT3/Functions.cpp
+++ MT3/Functions.cpp
@@ -22,7 +22,7 @@ Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float botto
 		{2.0f / (right - left),           0.0f,                           0.0f,                           0.0f},
 		{0.0f,                            2.0f / (top - bottom),          0.0f,                           0.0f},
 		{0.0f,                            0.0f,                           1.0f / (farClip - nearClip),    0.0f},
-		{ (left + right) / (left - right),(top + bottom) / (bottom - top),nearClip / (farClip - nearClip),1.0f}
+		{ (left + right) / (left - right),(top + bottom) / (bottom - top),nearClip / (nearClip - farClip),1.0f}
 	} };
 
 	return result;
